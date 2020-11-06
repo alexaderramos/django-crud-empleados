9356ce97730c132d0e71d1e5d1cb8402fece8868
Alexander R <tcristian_15.go@hotmail.com>
2020-11-05 20:58:06 -0500

    staticfiles en prod

--- empleado/settings/prod.py
+++ empleado/settings/prod.py
@@ -5,7 +5,7 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
@@ -23,6 +23,7 @@ DATABASES = {
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR.child('static')]
+STATIC_ROOT = BASE_DIR.child('staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR.child('media')
